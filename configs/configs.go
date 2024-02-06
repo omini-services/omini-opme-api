ebf169fd3cbc911fbf23284db876958f09684a15
Danilo Kasparian <dan.acceto@gmail.com>
2024-02-05 21:18:49 -0500

    fix config

--- configs/configs.go
+++ configs/configs.go
@@ -3,16 +3,15 @@ package configs
 import "github.com/spf13/viper"
 
 type conf struct {
-	DBConnectionString string `mapstructure:"ConnectionStrings__DefaultDb"`
+	DBConnectionString string `mapstructure:"DB_CONNECTION_STRING"`
 	WebServerPort      string `mapstructure:"WEB_SERVER_PORT"`
 }
 
 func LoadConfig(path string) (*conf, error) {
 	var cfg *conf
-	viper.SetConfigName("app_config")
-	viper.SetConfigType("env")
 	viper.AddConfigPath(path)
 	viper.SetConfigFile(".env")
+	viper.SetConfigType("env")
 	viper.AutomaticEnv()
 	err := viper.ReadInConfig()
 	if err != nil {
