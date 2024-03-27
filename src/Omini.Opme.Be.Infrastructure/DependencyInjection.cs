8aa3ab105d7c14461c5b10fe48cb102cb5e2dff2
Danilo Kasparian <dan.acceto@gmail.com>
2024-03-27 00:01:58 -0300

    init migration to csharp

---
+++ src/Omini.Opme.Be.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,9 @@
+namespace Omini.Opme.Be.Infrastructure;
+
+public static class DependecyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+
+    }
+}
---
+++ src/Omini.Opme.Be.Shared/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Omini.Opme.Be.Shared;
+
+public class Class1
+{
+
+}
