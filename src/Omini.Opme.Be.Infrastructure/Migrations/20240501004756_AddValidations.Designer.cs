18358bf58fc4dd64f67739d8fca43e28c74a589f
Danilo Kasparian <dan.acceto@gmail.com>
2024-04-30 20:48:17 -0400

    add maxlength

--- src/Omini.Opme.Be.Infrastructure/Migrations/20240501004756_AddValidations.Designer.cs
+++ src/Omini.Opme.Be.Infrastructure/Migrations/20240501004756_AddValidations.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Omini.Opme.Be.Infrastructure.Contexts;
@@ -11,9 +12,11 @@ using Omini.Opme.Be.Infrastructure.Contexts;
 namespace Omini.Opme.Be.Infrastructure.Migrations
 {
     [DbContext(typeof(OpmeContext))]
-    partial class OpmeContextModelSnapshot : ModelSnapshot
+    [Migration("20240501004756_AddValidations")]
+    partial class AddValidations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Omini.Opme.Be.Infrastructure/Migrations/20240501004756_AddValidations.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Omini.Opme.Be.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddValidations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "Patients",
+                type: "character varying(14)",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cnpj",
+                table: "InsuranceCompanies",
+                type: "character varying(18)",
+                maxLength: 18,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cnpj",
+                table: "Hospitals",
+                type: "character varying(18)",
+                maxLength: 18,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "Patients",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(14)",
+                oldMaxLength: 14);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cnpj",
+                table: "InsuranceCompanies",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(18)",
+                oldMaxLength: 18);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cnpj",
+                table: "Hospitals",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(18)",
+                oldMaxLength: 18);
+        }
+    }
+}
