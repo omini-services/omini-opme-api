6cbe3d388898241e7eb21a423d25359c574b040a
Danilo Kasparian <dan.acceto@gmail.com>
2024-05-02 22:14:42 -0400

    add new migration

---
+++ src/Omini.Opme.Be.Infrastructure/Migrations/20240503021430_addIdentityMapping.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Omini.Opme.Be.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIdentityMapping : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedBy",
+                table: "IdentityOpmeUsers",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "IdentityOpmeUsers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "IdentityOpmeUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DeletedBy",
+                table: "IdentityOpmeUsers",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "IdentityOpmeUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModified",
+                table: "IdentityOpmeUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "LastModifiedBy",
+                table: "IdentityOpmeUsers",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "IdentityOpmeUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "IdentityOpmeUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "IdentityOpmeUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "IdentityOpmeUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "IdentityOpmeUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastModified",
+                table: "IdentityOpmeUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "IdentityOpmeUsers");
+        }
+    }
+}
