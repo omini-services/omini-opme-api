a66a32eec7011548cab9bc829b2a3c0716acf864
Danilo Kasparian <dan.acceto@gmail.com>
2024-06-18 22:54:19 -0400

    fix add swa

--- src/Terraform/staticwebapp.tf
+++ src/Terraform/staticwebapp.tf
@@ -1,4 +1,4 @@
-resource "azurerm_static_web_app" "staticwebapp" {
+resource "azurerm_static_site" "staticwebapp" {
   name                = "appswa-omini-opme-eastus"
   resource_group_name = azurerm_resource_group.rg_app.name
   location            = azurerm_resource_group.rg_app.location
