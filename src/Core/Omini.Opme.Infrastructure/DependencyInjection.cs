3903a8bdafaacd17efe71fff0abc9e317354c987
Danilo Kasparian <dan.acceto@gmail.com>
2024-06-28 19:59:30 -0400

    fix connstring

--- src/Core/Omini.Opme.Infrastructure/DependencyInjection.cs
+++ src/Core/Omini.Opme.Infrastructure/DependencyInjection.cs
@@ -30,7 +30,7 @@ public static class DependecyInjection
                 sp.GetRequiredService<AuditableInterceptor>(),
                 sp.GetRequiredService<SoftDeletableInterceptor>()
             );
-            opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            opt.UseNpgsql(configuration.GetConnectionString("CUSTOMCONNSTR_DefaultConnection"));
         });
 
         services.AddScoped<IDateTimeService, DateTimeService>();
